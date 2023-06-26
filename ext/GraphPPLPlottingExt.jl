0d3c2084ab469c0b34e39f3f1c839820970752c7
Wouter Nuijten <wouternuijten@gmail.com>
2023-06-26 10:27:20 +0200

    Update plotting extension to be consistent with GraphPlot

--- ext/GraphPPLPlottingExt.jl
+++ ext/GraphPPLPlottingExt.jl
@@ -1,15 +1,14 @@
 module GraphPPLPlottingExt
 
-using GraphPPL, GraphPlot, Cairo, Compose, MetaGraphsNext
+using GraphPPL, GraphPlot, Cairo, Compose
 
-function GraphPPL.plot_graph(g::MetaGraph; file_name = "tmp.png")
+function GraphPlot.gplot(model::GraphPPL.Model; file_name = "tmp.png")
+    g = model.graph
     node_labels =
         [label[2].name for label in sort(collect(g.vertex_labels), by = x -> x[1])]
     plt = gplot(g, nodelabel = node_labels)
     draw(PNG(file_name, 16cm, 16cm), plt)
     return plt
 end
-
-GraphPPL.plot_graph(g::GraphPPL.Model; file_name = "tmp.png") = GraphPPL.plot_graph(g.graph; file_name = file_name)
 
 end
