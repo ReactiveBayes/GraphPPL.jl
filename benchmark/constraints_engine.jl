702fe8441cee042ccf70dfa5c077e7db70b4ea20
Wouter Nuijten <wouternuijten@gmail.com>
2023-07-10 10:07:00 +0200

    Add deepcopy calls to test in-place functions

--- benchmark/constraints_engine.jl
+++ benchmark/constraints_engine.jl
@@ -7,14 +7,14 @@ function benchmark_constraints_engine()
     SUITE = BenchmarkGroup()
     model = create_hgf(5)
     c = gethgfconstraints()
-    SUITE["apply_constraints_hgf"] = @benchmarkable GraphPPL.apply!(m, $c) setup=(m=$model) evals=1
+    SUITE["apply_constraints_hgf"] = @benchmarkable GraphPPL.apply!(m, $c) setup=(m=deepcopy($model)) evals=1
 
     for j in 10:5:20
         model = create_longarray(j)
         c = longarrayconstraints()
-        SUITE["apply meanfield constraint to vector of $j variables"]  = @benchmarkable GraphPPL.apply!(m, $c) setup=(m=$model) evals=1
+        SUITE["apply meanfield constraint to vector of $j variables"]  = @benchmarkable GraphPPL.apply!(m, $c) setup=(m=deepcopy($model)) evals=1
         GraphPPL.apply!(model, c)
-        SUITE["materialze constraints in vector of $j variables"] =  @benchmarkable GraphPPL.materialize_constraints!(m) setup=(m=$model) evals=1
+        SUITE["materialze constraints in vector of $j variables"] =  @benchmarkable GraphPPL.materialize_constraints!(m) setup=(m=deepcopy($model)) evals=1
     end
     return SUITE
 end
