83f3dadfcd75fa98fb90a5ad6164638d7f7d7bd7
Wouter Nuijten <wouternuijten@gmail.com>
2023-10-11 15:38:20 +0200

    Changed benchmarks to larger scale

--- benchmark/constraints_engine.jl
+++ benchmark/constraints_engine.jl
@@ -9,13 +9,11 @@ function benchmark_constraints_engine()
     c = gethgfconstraints()
     SUITE["apply_constraints_hgf"] = @benchmarkable GraphPPL.apply!(m, $c) setup=(m=deepcopy($model)) evals=1
 
-    for j in 10:5:20
-        model = create_longarray(j)
+    for j in 2:4
+        n_nodes = 10^j
+        model = create_longarray(n_nodes)
         c = longarrayconstraints()
-        SUITE["apply meanfield constraint to vector of $j variables"]  = @benchmarkable GraphPPL.apply!(m, $c) setup=(m=deepcopy($model)) evals=1
+        SUITE["apply meanfield constraint to vector of $n_nodes variables"]  = @benchmarkable GraphPPL.apply!(m, $c) setup=(m=deepcopy($model)) evals=1
     end
-    model = create_longarray(1000)
-    c = longarrayconstraints()
-    SUITE["apply meanfield constraint to vector of 1000 variables"]  = @benchmarkable GraphPPL.apply!(m, $c) setup=(m=deepcopy($model)) evals=1
     return SUITE
 end
