0c63455c0b515f7d64373e159fe860f1d3840299
Bagaev Dmitry <bvdmitri@gmail.com>
2024-02-15 16:13:16 +0100

    Prettify benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -3,8 +3,7 @@ using BenchmarkTools
 const SUITE = BenchmarkGroup()
 
 include("graph_engine.jl")
-# include("model_creation.jl")
-# include("constraints_engine.jl")
-# SUITE["constraints_engine"] = benchmark_constraints_engine()
+include("model_creation.jl")
+
 SUITE["graph_engine"] = benchmark_graph_engine()
-# SUITE["model_creation"] = benchmark_model_creation()
+SUITE["model_creation"] = benchmark_model_creation()
--- benchmark/model_creation.jl
+++ benchmark/model_creation.jl
@@ -1,17 +1,105 @@
 using BenchmarkTools
 using GraphPPL
 
-
-include("model_zoo.jl")
-
 function benchmark_model_creation()
     SUITE = BenchmarkGroup()
-    for i in 10 .^ range(1, stop=3)
-        SUITE["create HGF of depth $i"] = @benchmarkable create_hgf($i)
-    end
-    for i in 10 .^ range(2, stop=6)
-        n_nodes = 10^i
-        SUITE["create model with array of length $i"] = @benchmarkable create_longarray($n_nodes)
-    end
+
+    SUITE["model creation"]["state space (length)"] = benchmark_state_space_model()
+    SUITE["model creation"]["hierarchical (depth)"] = benchmark_hierarchical_model()
+
     return SUITE
+end
+
+## State space model benchmarks
+
+function benchmark_state_space_model()
+    SUITE = BenchmarkGroup()
+
+    for n in 10 .^ range(1, stop = 5)
+        # This SUITE benchmarks how long it takes to create a state space model with length `n` and default constraints
+        SUITE["default constraints", n] = @benchmarkable create_state_space_model($n)
+        # This SUITE benchmarks how long it takes to create a state space model with length `n` and mean field constraints
+        SUITE["mean field constraints", n] = @benchmarkable create_state_space_model($n, constraints) setup = begin
+            constraints = state_space_mean_field_constraints()
+        end
+    end
+
+    return SUITE
+end
+
+@model function state_space_model(n)
+    μ ~ Normal(0, 1)
+    σ ~ Gamma(1, 1)
+    x[1] ~ Normal(μ, σ)
+    for i in 2:n
+        x[i] ~ Normal(x[i - 1], σ)
+    end
+end
+
+state_space_mean_field_constraints() = @constraints begin
+    q(x) = q(x[begin]) .. q(x[end])
+    q(x, σ) = q(x)q(σ)
+    q(μ, σ) = q(μ)q(σ)
+end
+
+function create_state_space_model(n::Int, constraints = nothing)
+    plugins = if isnothing(constraints)
+        GraphPPL.PluginsCollection()
+    else
+        GraphPPL.PluginsCollection(GraphPPL.VariationalConstraintsPlugin(constraints))
+    end
+    return GraphPPL.create_model(GraphPPL.with_plugins(state_space_model(n = n), plugins))
+end
+
+## Hierarchical model benchmarks
+
+function benchmark_hierarchical_model()
+    SUITE = BenchmarkGroup()
+
+    for depth in 10 .^ range(1, stop = 2)
+        # This SUITE benchmarks how long it takes to create a state space model with depth `n` and default constraints
+        SUITE["default constraints", depth] = @benchmarkable create_hierarchical_model($depth)
+        # This SUITE benchmarks how long it takes to create a state space model with depth `n` and mean field constraints
+        SUITE["mean field constraints", depth] = @benchmarkable create_hierarchical_model($depth, constraints) setup = begin
+            constraints = hierarchical_mean_field_constraints()
+        end
+    end
+
+    return SUITE
+end
+
+@model function gcv(κ, ω, θ, x, y)
+    log_σ := κ * ω + θ
+    σ := exp(log_σ)
+    y ~ Normal(x, σ)
+end
+
+@model function hgf(κ, ω, θ, x_begin, depth)
+    means[1] ~ gcv(κ = κ, ω = ω, θ = θ, x = x_begin)
+    for i in 2:depth
+        means[i] ~ gcv(κ = κ, ω = ω, θ = θ, x = means[i - 1])
+    end
+end
+
+hierarchical_mean_field_constraints() = @constraints begin
+    q(means) = q(means[begin]) .. q(means[end])
+    for q in gcv
+        q(σ, log_σ) = q(σ)q(log_σ)
+        q(x, σ, y) = q(x, y)q(σ)
+    end
+end
+
+function create_hierarchical_model(depth::Int, constraints = nothing)
+    plugins = if isnothing(constraints)
+        GraphPPL.PluginsCollection()
+    else
+        GraphPPL.PluginsCollection(GraphPPL.VariationalConstraintsPlugin(constraints))
+    end
+    return GraphPPL.create_model(GraphPPL.with_plugins(hgf(depth = depth), plugins)) do model, ctx
+        κ = GraphPPL.getorcreate!(model, ctx, :κ, nothing)
+        ω = GraphPPL.getorcreate!(model, ctx, :ω, nothing)
+        θ = GraphPPL.getorcreate!(model, ctx, :θ, nothing)
+        x_begin = GraphPPL.getorcreate!(model, ctx, :x_begin, nothing)
+        return (; κ = κ, ω = ω, θ = θ, x_begin = x_begin)
+    end
 end
