1160917a33faa37930543015defc9ebb6a427b82
Bagaev Dmitry <bvdmitri@gmail.com>
2024-03-12 15:48:39 +0100

    2prev

--- src/graph_engine.jl
+++ src/graph_engine.jl
@@ -1021,6 +1021,13 @@ end
 # We cannot really check any `indices` if the underlying collection is missing 
 __lazy_node_label_check_variate_compatability(label::LazyNodeLabel, collection::MissingCollection, indices) = true
 
+
+# We know in advance that numbers cannot be queried with non-empty indices, so the error is thrown 
+# unless the indices are `(nothing, )`
+__lazy_node_label_check_variate_compatability(label::LazyNodeLabel, collection::Number, indices::Tuple) =
+    error(BoundsError(label.name, indices))
+__lazy_node_label_check_variate_compatability(label::LazyNodeLabel, collection::Number, ::Tuple{Nothing}) = true
+
 # Here we can check if the `indices` are compatible with the underlying collection
 function __lazy_node_label_check_variate_compatability(label::LazyNodeLabel, collection, indices)
     if !(checkbounds(Bool, collection, indices...)::Bool)
