808105094453c6baf2c46af094adf61f71d12f95
Wouter Nuijten <wouternuijten@gmail.com>
2024-03-18 14:32:36 +0100

    Update `check_variate_compatability` implementation

--- src/graph_engine.jl
+++ src/graph_engine.jl
@@ -881,12 +881,9 @@ check_variate_compatability(node::NodeLabel, index) =
 
 check_variate_compatability(label::GraphPPL.ProxyLabel, index) = check_variate_compatability(unroll(label), index)
 
-function check_variate_compatability(node::ResizableArray{NodeLabel, V, N}, index...) where {V, N}
-    if !(length(index) == N)
-        error("Index of length $(length(index)) not possible for $N-dimensional vector of random variables")
-    end
-    return isassigned(node, index...)
-end
+check_variate_compatability(node::ResizableArray{NodeLabel, V, N}, index::Vararg{Int, N}) where {V, N} = isassigned(node, index...)
+check_variate_compatability(node::ResizableArray{NodeLabel, V, N}, index::Vararg{Int, M}) where {V, N, M} = error("Index of length $(length(index)) not possible for $N-dimensional vector of random variables")
+
 
 check_variate_compatability(node::ResizableArray{NodeLabel, V, N}, index::Nothing) where {V, N} =
     error("Cannot call vector of random variables on the left-hand-side by an unindexed statement")
