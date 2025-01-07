c227bc643bf042dbe2d140e018e3124bcc198b89
Wouter Nuijten <wouternuijten@gmail.com>
2025-01-07 13:31:32 +0100

    improve benchmark infrastructure

--- scripts/bench.jl
+++ scripts/bench.jl
@@ -1,11 +1,16 @@
 using GraphPPL
 using BenchmarkTools
 using PkgBenchmark
+using Dates
 
-result, name = if ARGS == []
-    PkgBenchmark.benchmarkpkg(GraphPPL), "current"
+mkpath("./benchmark_logs")
+
+if isempty(ARGS)
+    result = PkgBenchmark.benchmarkpkg(GraphPPL)
+    export_markdown("./benchmark_logs/benchmark_$(now()).md", result)
+    export_markdown("./benchmark_logs/last.md", result)
 else
-    BenchmarkTools.judge(GraphPPL, ARGS[1]; judgekwargs = Dict(:time_tolerance => 0.1, :memory_tolerance => 0.05)), ARGS[1]
+    name = first(ARGS)
+    BenchmarkTools.judge(GraphPPL, name; judgekwargs = Dict(:time_tolerance => 0.1, :memory_tolerance => 0.05))
+    export_markdown("benchmark_vs_$(name)_result.md", result)
 end
-
-export_markdown("benchmark_vs_$(name)_result.md", result)
