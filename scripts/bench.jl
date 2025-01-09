4dab3d23ed4e842b077d4a0ee3b53593e9cf9cb3
Wouter Nuijten <wouternuijten@gmail.com>
2025-01-09 11:49:25 +0100

    Fix bug in benchmark script

--- scripts/bench.jl
+++ scripts/bench.jl
@@ -11,6 +11,6 @@ if isempty(ARGS)
     export_markdown("./benchmark_logs/last.md", result)
 else
     name = first(ARGS)
-    BenchmarkTools.judge(GraphPPL, name; judgekwargs = Dict(:time_tolerance => 0.1, :memory_tolerance => 0.05))
-    export_markdown("benchmark_vs_$(name)_result.md", result)
+    result = BenchmarkTools.judge(GraphPPL, name; judgekwargs = Dict(:time_tolerance => 0.1, :memory_tolerance => 0.05))
+    export_markdown("./benchmark_logs/benchmark_vs_$(name)_result.md", result)
 end
