2d921b055ed267a0ceb9fdaa032d3a2918ac1a16
Bagaev Dmitry <bvdmitri@gmail.com>
2025-05-23 17:41:17 +0200

    refine node label interface

--- src/interfaces/model_interface.jl
+++ src/interfaces/model_interface.jl
@@ -10,31 +10,22 @@ regardless of the underlying graph representation.
 abstract type FactorGraphModelInterface end
 
 """
-    NodeLabelInterface
+    VariableNodeLabel(label::Int)
 
-Abstract interface for node labels in a factor graph. This serves as a unified
-reference type for both variable and factor nodes, providing a consistent way
-to identify nodes regardless of implementation details.
+A node label for a variable node in a factor graph. A simple wrapper around an `Int`.
 """
-abstract type NodeLabelInterface end
-
-"""
-    VariableNodeLabelInterface
-
-Abstract interface for variable node labels in a factor graph. This serves as a unified
-reference type for both variable and factor nodes, providing a consistent way
-to identify nodes regardless of implementation details.
-"""
-abstract type VariableNodeLabelInterface <: NodeLabelInterface end
+struct VariableNodeLabel
+    label::Int
+end
 
 """
-    FactorNodeLabelInterface
+    FactorNodeLabel(label::Int)
 
-Abstract interface for factor node labels in a factor graph. This serves as a unified
-reference type for both variable and factor nodes, providing a consistent way
-to identify nodes regardless of implementation details.
+A node label for a factor node in a factor graph. A simple wrapper around an `Int`.
 """
-abstract type FactorNodeLabelInterface <: NodeLabelInterface end
+struct FactorNodeLabel
+    label::Int
+end
 
 """
     get_context(model::M) where {M<:FactorGraphModelInterface}
@@ -72,38 +63,12 @@ Implementations must extend this method from Graphs.jl.
 """
 Graphs.ne(model::M) where {M <: FactorGraphModelInterface} = throw(GraphPPLInterfaceNotImplemented(Graphs.ne, M, FactorGraphModelInterface))
 
-# Node Label Operations
-
-"""
-    is_variable_node(model::M, label::NodeLabelInterface) where {M<:FactorGraphModelInterface}
-
-Check if the node label refers to a variable node in the model.
-
-Returns a boolean indicating if the node is a variable node.
-"""
-function is_variable_node(model::M, label::NodeLabelInterface) where {M <: FactorGraphModelInterface}
-    throw(GraphPPLInterfaceNotImplemented(is_variable_node, M, FactorGraphModelInterface))
-end
-
-"""
-    is_factor_node(model::M, label::NodeLabelInterface) where {M<:FactorGraphModelInterface}
-
-Check if the node label refers to a factor node in the model.
-
-Returns a boolean indicating if the node is a factor node.
-"""
-function is_factor_node(model::M, label::NodeLabelInterface) where {M <: FactorGraphModelInterface}
-    throw(GraphPPLInterfaceNotImplemented(is_factor_node, M, FactorGraphModelInterface))
-end
-
 # Core Model Operations
 
 """
     add_variable!(model::M, data::VariableNodeDataInterface) where {M<:FactorGraphModelInterface}
 
-Add a variable node to the model with the given data.
-
-Returns the node label for the added variable.
+Add a variable node to the model with the given data. Returns the `VariableNodeLabel` for the added variable.
 """
 function add_variable!(model::M, data::VariableNodeDataInterface) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(add_variable!, M, FactorGraphModelInterface))
@@ -112,9 +77,7 @@ end
 """
     add_factor!(model::M, data::FactorNodeDataInterface) where {M<:FactorGraphModelInterface}
 
-Add a factor node to the model with the given data.
-
-Returns the node label for the added factor.
+Add a factor node to the model with the given data. Returns the `FactorNodeLabel` for the added factor.
 """
 function add_factor!(model::M, data::FactorNodeDataInterface) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(add_factor!, M, FactorGraphModelInterface))
@@ -124,8 +87,7 @@ end
     add_edge!(model::M, source::NodeLabelInterface, destination::NodeLabelInterface, edge_data::EdgeDataInterface) where {M<:FactorGraphModelInterface}
 
 Connect two nodes in the model with an edge containing the specified data.
-
-Returns a reference to the created edge.
+Returns a boolean indicating whether the edge was added successfully or not.
 """
 function add_edge!(
     model::M, source::NodeLabelInterface, destination::NodeLabelInterface, edge_data::EdgeDataInterface
@@ -134,22 +96,19 @@ function add_edge!(
 end
 
 """
-    has_edge(model::M, source::NodeLabelInterface, destination::NodeLabelInterface) where {M<:FactorGraphModelInterface}
+    has_edge(model::M, variable::VariableNodeLabel, factor::FactorNodeLabel) where {M<:FactorGraphModelInterface}
 
-Check if the model has an edge between the specified source and destination nodes.
-
+Check if the model has an edge between the specified variable and factor nodes.
 Returns a boolean indicating presence of the edge.
 """
-function has_edge(model::M, source::NodeLabelInterface, destination::NodeLabelInterface) where {M <: FactorGraphModelInterface}
+function has_edge(model::M, variable::VariableNodeLabel, factor::FactorNodeLabel) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(has_edge, M, FactorGraphModelInterface))
 end
 
 """
     get_variables(model::M) where {M<:FactorGraphModelInterface}
 
-Get all variable nodes in the model.
-
-Returns an iterable of node labels representing all variable nodes in the model.
+Get all variable nodes in the model. Returns an iterable of `VariableNodeLabel`s representing all variable nodes in the model.
 """
 function get_variables(model::M) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(get_variables, M, FactorGraphModelInterface))
@@ -158,68 +117,58 @@ end
 """
     get_factors(model::M) where {M<:FactorGraphModelInterface}
 
-Get all factor nodes in the model.
-
-Returns an iterable of node labels representing all factor nodes in the model.
+Get all factor nodes in the model. Returns an iterable of `FactorNodeLabel`s representing all factor nodes in the model.
 """
 function get_factors(model::M) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(get_factors, M, FactorGraphModelInterface))
 end
 
 """
-    get_variable_data(model::M, label::NodeLabelInterface) where {M<:FactorGraphModelInterface}
+    get_variable_data(model::M, label::VariableNodeLabel) where {M<:FactorGraphModelInterface}
 
 Get the variable node data identified by the given label.
 Will throw an error if the label does not refer to a variable node.
-
-Returns the variable node data if found, or nothing.
 """
-function get_variable_data(model::M, label::NodeLabelInterface) where {M <: FactorGraphModelInterface}
+function get_variable_data(model::M, label::VariableNodeLabel) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(get_variable_data, M, FactorGraphModelInterface))
 end
 
 """
-    get_factor_data(model::M, label::NodeLabelInterface) where {M<:FactorGraphModelInterface}
+    get_factor_data(model::M, label::FactorNodeLabel) where {M<:FactorGraphModelInterface}
 
 Get the factor node data identified by the given label.
 Will throw an error if the label does not refer to a factor node.
-
-Returns the factor node data if found, or nothing.
 """
-function get_factor_data(model::M, label::NodeLabelInterface) where {M <: FactorGraphModelInterface}
+function get_factor_data(model::M, label::FactorNodeLabel) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(get_factor_data, M, FactorGraphModelInterface))
 end
 
 """
-    get_edge_data(model::M, source::NodeLabelInterface, destination::NodeLabelInterface) where {M<:FactorGraphModelInterface}
+    get_edge_data(model::M, variable::VariableNodeLabel, factor::FactorNodeLabel) where {M<:FactorGraphModelInterface}
 
-Get the data of the edge between the specified source and destination nodes.
-
-Returns the edge data if found, or nothing.
+Get the data of the edge between the specified variable and factor nodes.
 """
-function get_edge_data(model::M, source::NodeLabelInterface, destination::NodeLabelInterface) where {M <: FactorGraphModelInterface}
+function get_edge_data(model::M, variable::VariableNodeLabel, factor::FactorNodeLabel) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(get_edge_data, M, FactorGraphModelInterface))
 end
 
 """
-    variable_neighbors(model::M, label::NodeLabelInterface) where {M<:FactorGraphModelInterface}
+    variable_neighbors(model::M, label::VariableNodeLabel) where {M<:FactorGraphModelInterface}
 
 Get all neighboring variable nodes of the specified factor node.
-
-Returns an iterable of node labels.
+Returns an iterable of `VariableNodeLabel`s.
 """
-function variable_neighbors(model::M, label::NodeLabelInterface) where {M <: FactorGraphModelInterface}
+function variable_neighbors(model::M, label::VariableNodeLabel) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(variable_neighbors, M, FactorGraphModelInterface))
 end
 
 """
-    factor_neighbors(model::M, label::NodeLabelInterface) where {M<:FactorGraphModelInterface}
+    factor_neighbors(model::M, label::FactorNodeLabel) where {M<:FactorGraphModelInterface}
 
 Get all neighboring factor nodes of the specified variable node.
-
-Returns an iterable of node labels.
+Returns an iterable of `FactorNodeLabel`s.
 """
-function factor_neighbors(model::M, label::NodeLabelInterface) where {M <: FactorGraphModelInterface}
+function factor_neighbors(model::M, label::FactorNodeLabel) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(factor_neighbors, M, FactorGraphModelInterface))
 end
 
@@ -227,8 +176,6 @@ end
     get_backend(model::M) where {M<:FactorGraphModelInterface}
 
 Get the backend associated with the model.
-
-Returns the backend object used by the model for node behavior and type definitions.
 """
 function get_backend(model::M) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(get_backend, M, FactorGraphModelInterface))
@@ -238,8 +185,6 @@ end
     get_plugins(model::M) where {M<:FactorGraphModelInterface}
 
 Get the plugins collection associated with the model.
-
-Returns the plugins collection object that contains all plugins enabled for the model.
 """
 function get_plugins(model::M) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(get_plugins, M, FactorGraphModelInterface))
@@ -249,9 +194,6 @@ end
     get_source_code(model::M) where {M<:FactorGraphModelInterface}
 
 Get the source code representation of the model.
-
-Returns the source object (typically a String) representing the original source code
-from which the model was created.
 """
 function get_source_code(model::M) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(get_source_code, M, FactorGraphModelInterface))
@@ -283,43 +225,6 @@ Must return a type that implements VariableNodeDataInterface.
 """
 function get_variable_data_type(model::M) where {M <: FactorGraphModelInterface}
     throw(GraphPPLInterfaceNotImplemented(get_variable_data_type, M, FactorGraphModelInterface))
-end
-
-"""
-    create_variable_data(model::M, context::Any, name::Symbol, index::Any;
-                      kind::Symbol=:random,
-                      link::Any=nothing,
-                      value::Any=nothing,
-                      metadata::Any=nothing) where {M<:FactorGraphModelInterface}
-
-Creates variable node data for the given model type by first resolving the appropriate
-node data type via get_variable_node_type and then constructing it with create_variable_node_data.
-
-# Arguments
-- `model::M`: The model interface implementation instance
-- `context::Any`: The context for this variable
-- `name::Symbol`: The name of the variable
-- `index::Any`: The index for this variable (nothing for non-indexed)
-- `kind::Symbol`: The kind of variable (defaults to :random)
-- `link::Any`: Optional link to other nodes/components
-- `value::Any`: Optional pre-assigned value
-- `metadata::Any`: Optional additional metadata for extensions
-
-# Returns
-A concrete implementation of VariableNodeDataInterface appropriate for the model type
-"""
-function create_variable_data(
-    model::M,
-    context::Any,
-    name::Symbol,
-    index::Any;
-    kind::Symbol = :random,
-    link::Any = nothing,
-    value::Any = nothing,
-    metadata::Any = nothing
-) where {M <: FactorGraphModelInterface}
-    T = get_variable_data_type(model)
-    return create_variable_data(T, name, index, kind, link, value, context, metadata)
 end
 
 """
@@ -383,7 +288,6 @@ function create_edge_data(model::M, name::Symbol, index::Any) where {M <: Factor
     return create_edge_data(T, name, index)
 end
 
-# Graph Creation Operations
 """
     create_model(::Type{T}; plugins=default_plugins(), backend=default_backend(T), source=nothing) where {T <: FactorGraphModelInterface}
 
