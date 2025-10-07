ebc110250f43d6e31c5dc4da625980f9dfc85c76
Fons van der Plas <fonsvdplas@gmail.com>
2025-10-07 19:25:01 +0200

    Improve error for "wrong number of interfaces"

--- src/graph_engine.jl
+++ src/graph_engine.jl
@@ -1834,9 +1834,15 @@ function prepare_interfaces(model::Model, fform::F, lhs_interface, rhs_interface
 end
 
 function prepare_interfaces(::StaticInterfaces{I}, fform::F, lhs_interface, rhs_interfaces::NamedTuple) where {I, F}
-    if !(length(I) == 1)
+    if length(I) == 0
         error(
-            lazy"Expected only one missing interface, got $I of length $(length(I)) (node $fform with interfaces $(keys(rhs_interfaces)))"
+            lazy"Node '$(fform)' needs to be defined with one unspecified interface.\n\nNode '$(fform)' is currently specified with all interfaces: $(keys(rhs_interfaces)), but one needs to be left out."
+        )
+    elseif length(I) > 1
+        inner = :out in I ? " (possibly $(join(setdiff(I, (:out,)), " and ")))" : ""
+        n = "\n"
+        error(
+            lazy"Node '$(fform)' is defined with too few interfaces (arguments), you need $(length(I)-1) more$(inner).$(n)$(n)Currently specified interfaces are: $(keys(rhs_interfaces))."
         )
     end
     missing_interface = first(I)
